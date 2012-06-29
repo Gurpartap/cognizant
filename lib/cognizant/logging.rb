872950dc63bca2aa6779ef463799f9cfedb57d1e
Gurpartap Singh <contact@gurpartap.com>
2012-06-30 03:56:57 +0530

    Logging module with multiple adapters.

---
+++ lib/cognizant/logging.rb
@@ -0,0 +1,33 @@
+require "logger"
+
+module Cognizant
+  module Logging
+    extend self
+
+    def add_log_adapter(file)
+      @files ||= Array.new
+      @files << file
+      @logger = nil
+    end
+
+    def log
+      @files ||= Array.new($stdout)
+      @logger ||= Logger.new(Files.new(*@files))
+    end
+    alias :logger :log
+
+    class Files
+      def initialize(*files)
+        @files = files
+      end
+
+      def write(*args)
+        @files.each { |t| t.write(*args) }
+      end
+
+      def close
+        @files.each(&:close)
+      end
+    end
+  end
+end
