8587e476f7ba176ca3d7473682e50ddecf50cabc
Gurpartap Singh <contact@gurpartap.com>
2012-07-01 15:12:16 +0530

    Basic eventmachine based RPC

--- lib/cognizant/client/interface.rb
+++ lib/cognizant/client/interface.rb
@@ -1,7 +1,33 @@
+require "eventmachine"
+
 module Cognizant
   module Client
-    class Interface < EM::Connection
-      include EM::Protocols::SASLauthclient
+    class Interface
+      def initialize(server = nil)
+        @sock = EventMachine.connect(server, Client::Interface::Handler)
+      end
+
+      def method_missing(*meth, &blk)
+        @sock.queue << blk
+        @sock.send_object(meth)
+      end
+
+      class Handler < EventMachine::Connection
+        # include EventMachine::Protocols::SASLauthclient
+        include EventMachine::Protocols::ObjectProtocol
+
+        attr_reader :queue
+
+        def post_init
+          @queue = []
+        end
+
+        def receive_object(obj)
+          if callback = @queue.shift
+            callback.call(obj)
+          end
+        end
+      end
     end
   end
 end
--- lib/cognizant/server/interface.rb
+++ lib/cognizant/server/interface.rb
@@ -1,24 +1,28 @@
+require "eventmachine"
+
 module Cognizant
   module Server
     class Interface < EventMachine::Connection
-      include EM::Protocols::SASLauth
+      include EventMachine::Protocols::ObjectProtocol
+      # include EventMachine::Protocols::SASLauth
 
       def post_init
+        @obj = Hash.new
         puts "-- someone connected to the echo server!"
       end
 
-      def receive_data data
-        send_data ">>>you sent: #{data}"
-        close_connection if data =~ /quit/i
+      def receive_object(method)
+        send_object @obj.send(*method)
       end
 
       def unbind
+        @obj = nil
         puts "-- someone disconnected from the echo server!"
       end
 
-      def validate(usr, psw, sys, realm)
-        usr == TestUser and psw == TestPsw
-      end
+      # def validate(usr, psw, sys, realm)
+      #   usr == TestUser and psw == TestPsw
+      # end
     end
   end
 end
