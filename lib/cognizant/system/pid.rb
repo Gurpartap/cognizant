4c709e380d87c85efc1b12944b5f4f3d54e7e0e9
Gurpartap Singh <contact@gurpartap.com>
2012-07-02 17:13:38 +0530

    PID manager for processes.

---
+++ lib/cognizant/system/pid.rb
@@ -0,0 +1,30 @@
+require "cognizant/system/exec"
+
+module Cognizant
+  module System
+    module PID
+      def read_pid
+        if self.pid_command
+          str = System::Execute.command(self.pid_command).stdout.to_i
+          @process_pid = str unless not str or str.zero?
+        elsif self.pidfile and File.exists?(self.pidfile)
+          str = File.read(self.pidfile).to_i
+          @process_pid = str unless not str or str.zero?
+        end
+        @process_pid
+      end
+
+      def write_pid(pid = nil)
+        @process_pid = pid if pid
+        File.open(self.pidfile, "w") { |f| f.write(@process_pid) } if self.pidfile and @process_pid
+      end
+
+      def unlink_pid
+        File.unlink(self.pidfile) if self.pidfile
+      rescue Errno::ENOENT
+        # It got deleted before we could. Perhaps it was a process managed pidfile.
+        true
+      end
+    end
+  end
+end
