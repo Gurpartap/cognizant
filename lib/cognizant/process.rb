92aaa5b72a52b3ff6f510d8008210a272cda41ab
Gurpartap Singh <contact@gurpartap.com>
2012-07-02 15:26:36 +0530

    Add system operations.

--- lib/cognizant/process.rb
+++ lib/cognizant/process.rb
@@ -1,9 +1,11 @@
-require "cognizant/system/exec"
-require "cognizant/system/ps"
+require "cognizant/server/system/exec"
+require "cognizant/server/system/ps"
 
 module Cognizant
   module Server
     class Process
+      include System::PID
+
       # Unique name for the process.
       # @return [String]
       attr_accessor :name
@@ -197,6 +199,20 @@ module Cognizant
         after_transition any => :restarting, :do => :restart_process
       end
 
+      def initialize(options)
+        options.each do |attribute_name, value|
+          # Do not accept non configurable attributes.
+          unless [:ticks_to_skip].include?(attribute_name)
+            self.send("#{attribute_name}=", value) if self.respond_to?("#{attribute_name}=")
+          end
+        end
+
+        self.ticks_to_skip = 0
+
+        # Let state_machine initialize as well.
+        super
+      end
+
       def process_running?
         @process_running = begin
           # Do not assume change when we're giving time to an execution by skipping ticks.
@@ -216,6 +232,159 @@ module Cognizant
         self.autostart and not process_running?
       end
 
+      def pidfile
+        @pidfile = @pidfile || default_pid_file
+      end
+
+      def tick
+        return puts "(skip tick)" if skip_tick?
+        @action_thread.kill if @action_thread # Is this even required? Since we a have timer within the thread to destruct itself.
+        super
+      end
+
+      def start_process
+        result_handler = Proc.new do |result|
+          if result.respond_to?(:succeeded?) and result.succeeded?
+            write_pid(result.pid) if result.pid != 0
+          end
+        end
+        execute_action(
+          result_handler,
+          daemonize: self.daemonize,
+          env:       (self.env || {}).merge(self.start_env || {}),
+          before:    self.start_before_command,
+          command:   self.start_command,
+          after:     self.start_after_command,
+          timeout:   self.start_timeout
+        )
+      end
+
+      def stop_process
+        result_handler = Proc.new do |result|
+          # If it is a boolean and value is true OR if it's an execution result and it succeeded.
+          if (!!result == result and result) or (result.respond_to?(:succeeded?) and result.succeeded?)
+            unlink_pid unless alive?
+          end
+        end
+        execute_action(
+          result_handler,
+          env:     (self.env || {}).merge(self.stop_env || {}),
+          before:  self.stop_before_command,
+          command: self.stop_command,
+          signals: self.stop_signals,
+          after:   self.stop_after_command,
+          timeout: self.stop_timeout
+        )
+      end
+
+      def restart_process
+        result_handler = Proc.new do |result|
+          # If it is a boolean and value is true OR if it's an execution result and it succeeded.
+          if (!!result == result and result) or (result.respond_to?(:succeeded?) and result.succeeded?)
+            unlink_pid unless alive?
+          end
+        end
+        execute_action(
+          result_handler,
+          env:     (self.env || {}).merge(self.restart_env || {}),
+          before:  self.restart_before_command,
+          command: self.restart_command,
+          signals: self.restart_signals,
+          after:   self.restart_after_command,
+          timeout: self.restart_timeout
+        )
+      end
+
+      private
+
+      def default_pid_file
+        ""
+      end
+
+      def skip_ticks_for(seconds)
+        # 1 second = 1 skip
+        # We can optionally accept negative skip seconds totalling >= 0.
+        self.ticks_to_skip = [self.ticks_to_skip + (seconds.to_i + 1), 0].max # +1 so that we don't have to >= and ensure 0 in skip_tick?
+      end
+
+      def skip_tick?
+        (self.ticks_to_skip -= 1) > 0 if self.ticks_to_skip > 0
+      end
+
+      def run_options(overrides = {})
+        options = {}
+        # CONFIGURABLE_ATTRIBUTES.each do |o|
+        #   options[o] = self.send(o)
+        # end
+        options.merge(overrides)
+      end
+
+      def run(command, overrides = {})
+        options = {}
+        options = run_options({ daemonize: false }.merge(overrides)) if overrides
+        System::Execute.command(command, options)
+      end
+
+      def execute_action(result_handler, options)
+        daemonize      = options[:daemonize] || false
+        env            = options[:env]
+        before_command = options[:before]
+        command        = options[:command]
+        after_command  = options[:after]
+        signals        = options[:signals]
+        timeout        = options[:timeout] || 10
+
+        # TODO: Works well but can some refactoring make it more declarative?
+        @action_thread = Thread.new do
+          result = false
+          queue = Queue.new
+          thread = Thread.new do
+            if before_command and not success = run(before_command).succeeded?
+              queue.push(success)
+              Thread.exit
+            end
+
+            if (command and success = run(command, { daemonize: daemonize, env: env }) and success.succeeded?)
+              run(after_command) if after_command
+              queue.push(success)
+              Thread.exit
+            end
+
+            # If the caller has attempted to set signals, then it can handle it's result.
+            if (options.has_key?(:signals) and success = System::Process.new(read_pid).stop(signals: signals, timeout: timeout))
+              run(after_command) if after_command
+              queue.push(success)
+              Thread.exit
+            end
+
+            queue.push(false)
+            Thread.exit
+          end
+
+          timeout_left = timeout + 1
+          while (timeout_left -= 1) > 0 do
+            # If there is something in the queue, we have the required result. Simpler than Mutex#synchronize.
+            if result = queue.pop
+              # Rollback the pending skips, since we finished before timeout.
+              skip_ticks_for(-timeout_left)
+              break
+            end
+            sleep 1
+          end
+
+          # Kill the nested thread.
+          thread.kill
+
+          # Action callback.
+          result_handler.call(result) if result_handler.respond_to?(:call)
+
+          # Kill self.
+          Thread.kill
+        end
+
+        # We skip so that we're not reinformed about the required transition by the tick.
+        skip_ticks_for(timeout)
+      end
     end
   end
 end
