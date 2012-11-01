aca6937c44ae1406ae665777a5ee18444b9e230a
Gurpartap Singh <contact@gurpartap.com>
2012-11-01 15:16:05 +0530

    Cleanup the main class methods.

--- lib/cognizant.rb
+++ lib/cognizant.rb
@@ -1,38 +1,7 @@
-require "cognizant/client/interface"
+require "cognizant/server"
 
 module Cognizant
-  def self.add_process(options)
-    EventMachine.run do
-      client = Cognizant::Client::Interface.new(options[:socket])
-      # client[:A] = 99
-      # client[:B] = 98
-      # client[:C] = 97
-      # client.keys   { |keys| puts "keys: #{keys}" }
-      # client.values { |vals| puts "vals: #{vals}" }
-    end
-  end
-
-  def self.monitor_process(*args)
-    
-  end
-
-  def self.start_process(*args)
-    
-  end
-
-  def self.stop_process(*args)
-    
-  end
-
-  def self.restart_process(*args)
-    
-  end
-
-  def self.unmonitor_process(*args)
-    
-  end
-
-  def self.remove_process(*args)
-    
+  def self.monitor(process_name, &block)
+    Cognizant::Server.daemon.monitor(process_name, &block)
   end
 end
