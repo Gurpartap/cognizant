bca46c3b9e27eec1ff0665c5fc5e368e1ab2d922
Gurpartap Singh <contact@gurpartap.com>
2012-11-23 00:21:41 +0530

    Comment out uid and gid from example, since by most wont have these on their systems.

--- examples/redis-server.rb
+++ examples/redis-server.rb
@@ -1,11 +1,11 @@
 # 4 slave instances to master at port 6000.
 5.times do |i|
   Cognizant.monitor "redis-server-600#{i}" do |process|
-    process.group            = "redis"
-    process.uid              = "redis"
-    process.gid              = "redis"
-    process.start_command    = "redis-server -"
-    process.ping_command     = "redis-cli -p 600#{i} PING"
+    process.group         = "redis"
+    # process.uid           = "redis"
+    # process.gid           = "redis"
+    process.start_command = "redis-server -"
+    process.ping_command  = "redis-cli -p 600#{i} PING"
 
     slaveof = i == 0 ? "" : "slaveof 127.0.0.1 6000"
     process.start_with_input = <<-heredoc
