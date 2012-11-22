be05c04885d650d3e21d3309001a790d619ed3bc
Gurpartap Singh <contact@gurpartap.com>
2012-11-23 00:32:15 +0530

    Support status information for individual process.

--- lib/cognizant/server/commands.rb
+++ lib/cognizant/server/commands.rb
@@ -9,27 +9,31 @@ module Cognizant
       end
 
       def self.status(*args)
-        if [*args].size == 1 and process_name = args.shift
+        output_processes = []
+        if args.size == 1 and process_name = args.shift
           Cognizant::Server.daemon.processes.each do |process|
             if process.name.eql?(process_name)
-              yield("#{process.name}: #{process.state}")
-              return # yield("OK")
+              output_processes << process
             end
-          end  
-          raise("ERR: No such process")
-          # yield("OK")
-        else
-          output = []
-          Cognizant::Server.daemon.processes.each do |process|
-            output << {
-              "Process" => process.name,
-              "Group"   => process.group,
-              "State"   => process.state
-            }
           end
-          yield(output.to_json)
-          # yield("OK")
+          if output_processes.size == 0
+            raise("ERR: No such process")
+            # yield("OK")
+          end
+        else
+          output_processes = Cognizant::Server.daemon.processes
         end
+        
+        output = []
+        output_processes.each do |process|
+          output << {
+            "Process" => process.name,
+            "Group"   => process.group,
+            "State"   => process.state
+          }
+        end
+        yield(output.to_json)
+        # yield("OK")
       end
 
       %w(monitor start stop restart unmonitor).each do |action|
