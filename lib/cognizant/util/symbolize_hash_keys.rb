4ef0124bb35970e4ae1d627c72fbeb2a85bd27c9
Gurpartap Singh <contact@gurpartap.com>
2012-11-24 03:55:27 +0530

    Add deep symbolizing util for Hash.

---
+++ lib/cognizant/util/symbolize_hash_keys.rb
@@ -0,0 +1,19 @@
+class Hash
+  # Destructively convert all keys by using the block operation.
+  # This includes the keys from the root hash and from all
+  # nested hashes.
+  def deep_transform_keys!(&block)
+    keys.each do |key|
+      value = delete(key)
+      self[yield(key)] = value.is_a?(Hash) ? value.deep_transform_keys!(&block) : value
+    end
+    self
+  end
+
+  # Destructively convert all keys to symbols, as long as they respond
+  # to +to_sym+. This includes the keys from the root hash and from all
+  # nested hashes.
+  def deep_symbolize_keys!
+    deep_transform_keys!{ |key| key.to_sym rescue key }
+  end
+end
