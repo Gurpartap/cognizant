85ffd4a44858528cea9edb8326d845f931b22d45
Gurpartap Singh <contact@gurpartap.com>
2012-11-24 02:09:24 +0530

    Add conditions to example.

--- examples/redis-server.rb
+++ examples/redis-server.rb
@@ -1,5 +1,5 @@
-# 4 slave instances to master at port 6000.
-5.times do |i|
+# 2 slave instances to master at port 6000.
+3.times do |i|
   Cognizant.monitor "redis-server-600#{i}" do |process|
     process.group         = "redis"
     # process.uid           = "redis"
@@ -13,5 +13,15 @@
       port 600#{i}
       #{slaveof}
     heredoc
+
+    # process.on :always_true, :every => 2.seconds, :times => 3 do |p|
+    #   `say "Boom!"`
+    # end
+
+    process.on :cpu_usage, :above => 60, :every => 3.seconds, :times => 3, :do => :restart
+
+    process.on :memory_usage, :above => 100.megabytes, :every => 5.seconds, :times => [3, 5] do |p|
+      p.restart # Restart is the default anyways.
+    end
   end
 end
