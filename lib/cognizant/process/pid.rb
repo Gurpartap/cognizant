a38da3a6d9010b842370afce9bd237e2c3af5015
Gurpartap Singh <contact@gurpartap.com>
2012-11-24 01:52:07 +0530

    Ensure that pid is valid after reading it from available source.

--- lib/cognizant/process/pid.rb
+++ lib/cognizant/process/pid.rb
@@ -4,11 +4,13 @@ module Cognizant
       def read_pid
         if self.pid_command
           str = execute(self.pid_command).stdout.to_i
-          @process_pid = str unless not str or str.zero? # TODO: Also check if the pid is alive.
+          @process_pid = str unless not str or str.zero?
+          # TODO: Write pid to pidfile, since our source was pid_command instead.
         elsif self.pidfile and File.exists?(self.pidfile)
           str = File.read(self.pidfile).to_i
-          @process_pid = str unless not str or str.zero? # TODO: Also check if the pid is alive.
+          @process_pid = str unless not str or str.zero?
         end
+        @process_pid = 0 unless System.pid_running?(@process_pid)
         @process_pid
       end
 
