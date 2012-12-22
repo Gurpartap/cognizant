6780d566af50762493d11b764f651dbd3b570e77
Gurpartap Singh <contact@gurpartap.com>
2012-12-23 03:02:12 +0530

    Add DSLProxy class to optionally allow easier dsl syntax for defining process information.

---
+++ lib/cognizant/process/dsl_proxy.rb
@@ -0,0 +1,31 @@
+module Cognizant
+  class Process
+    class DSLProxy
+      attr_accessor :attributes
+
+      def initialize(process, &dsl_block)
+        @process = process
+        @attributes = Hash.new
+        instance_eval(&dsl_block)
+      end
+
+      def method_missing(name, *args, &block)
+        if args.size == 1 and name.to_s =~ /^(.*)=$/
+          @attributes[$1.to_sym] = args.first
+        elsif args.size == 1
+          @attributes[name.to_sym] = args.first
+        elsif args.size == 0 and name.to_s =~ /^(.*)!$/
+          @attributes[$1.to_sym] = true
+        elsif args.empty? and @attributes.key?(name.to_sym)
+          @attributes[name.to_sym]
+        else
+          super
+        end
+      end
+
+      def check(condition_name, options, &block)
+        @process.check(condition_name, options, &block)
+      end
+    end
+  end
+end
