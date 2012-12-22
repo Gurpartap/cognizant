3ab05c2584e7cd588359419e9c140ecfb42e1eac
Gurpartap Singh <contact@gurpartap.com>
2012-12-23 03:08:57 +0530

    Add a DSL example.

---
+++ examples/redis-server_dsl.rb
@@ -0,0 +1,28 @@
+# 2 slave instances to master at port 6000.
+3.times do |i|
+  slaveof = i == 0 ? "" : "slaveof 127.0.0.1 6000" # Any custom code has to be outside the monitor block.
+
+  Cognizant.monitor do
+    name "redis-server-600#{i}"
+    group "redis"
+    start_command "redis-server -"
+    ping_command "redis-cli -p 600#{i} PING"
+
+    start_with_input <<-heredoc
+      daemonize no
+      port 600#{i}
+      #{slaveof}
+    heredoc
+
+    # check :always_true, :every => 2.seconds, :times => 3 do |p|
+    #   `say "Boom!"`
+    # end
+
+    check :flapping, :times => 5, :within => 30.seconds, :retry_after => 7.seconds
+
+    check :cpu_usage, :every => 3.seconds, :above => 60, :times => 3, :do => :restart
+    check :memory_usage, :every => 5.seconds, :above => 100.megabytes, :times => [3, 5] do |p|
+      p.restart # Restart is the default anyways.
+    end
+  end
+end
