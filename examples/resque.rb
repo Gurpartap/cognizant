6887cb3fccbf4d5f7c2a722c2704ae126399b88b
Gurpartap Singh <contact@gurpartap.com>
2012-12-23 09:28:00 +0530

    Add monitor_children example to resque example.

--- examples/resque.rb
+++ examples/resque.rb
@@ -6,5 +6,10 @@
     process.chdir         = "/apps/example/current/"
     process.env           = { "QUEUE" => "*", "RACK_ENV" => "production" }
     process.start_command = "bundle exec rake resque:work"
+
+    process.monitor_children do
+      check :memory_usage, :every => 5.seconds, :above => 100.megabytes, :times => [2, 3], :do => :stop
+      stop_signals ["TERM", "INT", "KILL"]
+    end
   end
 end
