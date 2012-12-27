80d7aff517dc10e472b4892a3e486b8249f5693e
Gurpartap Singh <contact@gurpartap.com>
2012-12-27 13:07:16 +0530

    Reorder schedule_events in flapping action for correct behavior.

--- lib/cognizant/process/triggers/flapping.rb
+++ lib/cognizant/process/triggers/flapping.rb
@@ -52,8 +52,8 @@ module Cognizant
 
             puts "Flapping detected (##{@num_of_tries}) for #{@delegate.process.name}(pid:#{@delegate.process.cached_pid})."
 
-            @delegate.schedule_event(:unmonitor, 0)
             @delegate.schedule_event(:start, self.retry_after) if can_retry?
+            @delegate.schedule_event(:unmonitor, 0)
 
             @timeline.clear
 
