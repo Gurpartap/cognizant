c012a4ea99ef7a2f929ee209df567038825b03aa
Gurpartap Singh <contact@gurpartap.com>
2013-03-18 17:37:04 +0530

    Reverting the separating out of Cognizant::Process state machine and conditions into different files.

--- lib/cognizant/process.rb
+++ lib/cognizant/process.rb
@@ -1,7 +1,8 @@
 require "monitor"
 require "thread"
 
-require "cognizant/process/state_machine"
+require "state_machine"
+
 require "cognizant/process/dsl_proxy"
 require "cognizant/process/pid"
 require "cognizant/process/status"
@@ -24,6 +25,67 @@ module Cognizant
     include Cognizant::Process::Actions
     include Cognizant::Process::Children
 
+    state_machine :initial => :unmonitored do
+      # These are the idle states, i.e. only an event (either external or internal) will trigger a transition.
+      # The distinction between stopped and unmonitored is that stopped
+      # means we know it is not running and unmonitored is that we don't care if it's running.
+      state :unmonitored, :running, :stopped
+
+      # These are transitionary states, we expect the process to change state after a certain period of time.
+      state :starting, :stopping, :restarting
+
+      event :tick do
+        transition :starting   => :running,   :if     => :process_running?
+        transition :starting   => :stopped,   :unless => :process_running?
+
+        transition :running    => :stopped,   :unless => :process_running?
+
+        # The process failed to die after entering the stopping state. Change the state to reflect reality.
+        transition :stopping   => :running,   :if     => :process_running?
+        transition :stopping   => :stopped,   :unless => :process_running?
+
+        transition :stopped    => :running,   :if     => :process_running?
+        transition :stopped    => :starting,  :if     => lambda { |p| p.autostart and not p.process_running? }
+
+        transition :restarting => :running,   :if     => :process_running?
+        transition :restarting => :stopped,   :unless => :process_running?
+      end
+
+      event :monitor do
+        transition :unmonitored => :stopped
+      end
+
+      event :start do
+        transition [:unmonitored, :stopped] => :starting
+      end
+
+      event :stop do
+        transition :running => :stopping
+      end
+
+      event :restart do
+        transition [:running, :stopped] => :restarting
+      end
+
+      event :unmonitor do
+        transition any => :unmonitored
+      end
+
+      before_transition any      => :starting,    :do => lambda { |p| p.autostart = true }
+      after_transition  any      => :starting,    :do => :start_process
+
+      before_transition any      => :stopping,    :do => lambda { |p| p.autostart = false }
+      after_transition  :running => :stopping,    :do => :stop_process
+
+      before_transition any      => :restarting,  :do => lambda { |p| p.autostart = true }
+      after_transition  any      => :restarting,  :do => :restart_process
+
+      before_transition any      => :unmonitored, :do => lambda { |p| p.autostart = false }
+
+      before_transition any => any, :do => :notify_triggers
+      after_transition  any => any, :do => :record_transition
+    end
+
     def initialize(process_name = nil, attributes = {}, &block)
       reset!
 
@@ -43,6 +105,7 @@ module Cognizant
       raise "Process name is missing. Aborting." unless self.name
       Log[self].info "Loading process #{self.name}..."
 
+      # Let state_machine initialize as well.
       initialize_state_machines
     end
 
@@ -58,9 +121,40 @@ module Cognizant
       @monitor_children = false
     end
 
+    def check(check_name, options, &block)
+      if klass = Cognizant::Process::Conditions[check_name]
+        @conditions << ConditionDelegate.new(check_name, options.deep_symbolize_keys!, &block)
+      elsif klass = Cognizant::Process::Triggers[check_name]
+        @triggers << TriggerDelegate.new(check_name, self, options.deep_symbolize_keys!, &block)
+      end
+    end
+
     def monitor_children(child_process_attributes = {}, &child_process_block)
       @monitor_children = true
       @child_process_attributes, @child_process_block = child_process_attributes, child_process_block
+    end
+
+    def tick
+      return if skip_tick?
+      @action_thread.kill if @action_thread # TODO: Ensure if this is really needed.
+
+      # Invoke the state_machine event.
+      super
+
+      if self.running? # State method.
+        run_conditions
+
+        if @monitor_children
+          refresh_children!
+          @children.each(&:tick)
+        end
+      end
+    end
+
+    def skip_ticks_for(skips)
+      # Accept negative skips with the result being >= 0.
+      # +1 so that we don't have to >= and ensure 0 in #skip_tick?.
+      @ticks_to_skip = [@ticks_to_skip + (skips.to_i + 1), 0].max
     end
 
     def pidfile
@@ -94,6 +188,25 @@ module Cognizant
 
     private
 
+    def record_transition(transition)
+      unless transition.loopback?
+        @transitioned = true
+        @last_transition_time = Time.now.to_i
+
+        # When a process changes state, we should clear the memory of all the conditions.
+        @conditions.each { |condition| condition.clear_history! }
+        Log[self].debug "Changing state of #{name} from #{transition.from_name} => #{transition.to_name}"
+
+        # And we should re-populate its child list.
+        if @monitor_children
+          @children.clear
+        end
+
+        # Update the pid from pidfile, since the state of process changed, if the process is managing it's own pidfile.
+        read_pid if @pidfile
+      end
+    end
+
     def set_attributes(attributes)
       if attributes.has_key?(:checks) and attributes[:checks].kind_of?(Hash)
         attributes[:checks].each do |check_name, args, &block|
@@ -111,6 +224,10 @@ module Cognizant
       end
     end
 
+    def skip_tick?
+      (@ticks_to_skip -= 1) > 0 if @ticks_to_skip > 0
+    end
+
     def run(command, action_overrides = {})
       options = { daemonize: false }
       # Options from daemon config.
@@ -118,6 +235,37 @@ module Cognizant
         options[attribute] = self.send(attribute)
       end
       execute(command, options.merge(action_overrides))
+    end
+
+    def run_conditions
+      now = Time.now.to_i
+
+      threads = @conditions.collect do |condition|
+        [condition, Thread.new { Thread.current[:actions] = condition.run(cached_pid, now) }]
+      end
+
+      @transitioned = false
+
+      collect_conditions_actions(threads).each do |(action, reason)|
+        break if @transitioned
+        dispatch!(action, reason)
+      end
+    end
+
+    def collect_conditions_actions(threads)
+      threads.inject([]) do |actions, (condition, thread)|
+        thread.join
+        thread[:actions].each do |action|
+          action_name = action.respond_to?(:call) ? "call to custom block" : action
+          Log[self].debug "Dispatching #{action_name} to #{name} for #{condition.to_s}."
+          actions << [action, condition.to_s]
+        end
+        actions
+      end
+    end
+
+    def notify_triggers(transition)
+      @triggers.each { |trigger| trigger.notify(transition) }
     end
   end
 end
--- lib/cognizant/process/conditions.rb
+++ lib/cognizant/process/conditions.rb
@@ -6,43 +6,6 @@ end
 
 module Cognizant
   class Process
-    def check(check_name, options, &block)
-      if klass = Cognizant::Process::Conditions[check_name]
-        @conditions << ConditionDelegate.new(check_name, options.deep_symbolize_keys!, &block)
-      elsif klass = Cognizant::Process::Triggers[check_name]
-        @triggers << TriggerDelegate.new(check_name, self, options.deep_symbolize_keys!, &block)
-      end
-    end
-
-    private
-
-    def run_conditions
-      now = Time.now.to_i
-
-      threads = @conditions.collect do |condition|
-        [condition, Thread.new { Thread.current[:actions] = condition.run(cached_pid, now) }]
-      end
-
-      @transitioned = false
-
-      collect_conditions_actions(threads).each do |(action, reason)|
-        break if @transitioned
-        dispatch!(action, reason)
-      end
-    end
-
-    def collect_conditions_actions(threads)
-      threads.inject([]) do |actions, (condition, thread)|
-        thread.join
-        thread[:actions].each do |action|
-          action_name = action.respond_to?(:call) ? "call to custom block" : action
-          Log[self].debug "Dispatching #{action_name} to #{name} for #{condition.to_s}."
-          actions << [action, condition.to_s]
-        end
-        actions
-      end
-    end
-
     module Conditions
       def self.[](name)
         begin
